1cdc034d8fac47f8c783ddcbec764a71f2891e5e
NataliaColome <nmcolome@gmail.com>
2020-03-06 13:13:36 -0700

    Add /api/v1/invoice_items#show route & action

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::InvoiceItemsController < ApplicationController
   def index
     render json: InvoiceItemsSerializer.new(InvoiceItem.all)
   end
+
+  def show
+    render json: InvoiceItemsSerializer.new(InvoiceItem.find(params[:id]))
+  end
 end
