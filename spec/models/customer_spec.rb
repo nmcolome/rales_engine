5e1305afff9f167c50b5a8ce69e5ff78d8970a9e
NataliaColome <nmcolome@gmail.com>
2020-03-05 23:53:39 -0700

    Test & add associations from join tables (InvoiceItems & Invoices)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Customer, type: :model do
 
   describe "relationships" do
     it { should have_many :invoices }
+    it { should have_many(:merchants).through(:invoices) }
   end
 end
