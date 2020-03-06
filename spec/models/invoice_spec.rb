cc12dd35f99b4f8e97d340e1cbc2d61b31cee241
NataliaColome <nmcolome@gmail.com>
2020-03-05 23:35:44 -0700

    generate Transaction model, test & add relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Invoice, type: :model do
   describe "relationships" do
     it { should belong_to :customer }
     it { should belong_to :merchant }
+    it { should have_many :transactions }
   end
 
   describe "enums" do
