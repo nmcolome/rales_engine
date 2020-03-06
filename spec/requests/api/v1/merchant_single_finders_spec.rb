e4c37fef228e83218ccda43ee75c33b73217348f
NataliaColome <nmcolome@gmail.com>
2020-03-06 14:45:27 -0700

    Add request spec for merchant single finder

---
+++ spec/requests/api/v1/merchant_single_finders_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Merchant find endpoints" do
+  describe "returns a single merchant based on any its attributes" do
+    it "finds based on id" do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/find?id=#{id}"
+
+      expect(response).to be_successful
+
+      raw = JSON.parse(repsonse.body)
+      merchant = raw["data"]
+
+      expect(merchant["id"].to_s).to eq(id)
+    end
+
+    it "finds based on name" do
+      name = create(:merchant).name
+
+      get "/api/v1/merchants/find?name=#{name}"
+
+      expect(response).to be_successful
+
+      raw = JSON.parse(repsonse.body)
+      merchant = raw["data"]
+
+      expect(merchant["attributes"]["name"]).to eq(name)
+    end
+  end
+end
