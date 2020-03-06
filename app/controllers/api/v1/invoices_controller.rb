ec76d3a044e295d0a267e653d0539e23e1b58136
NataliaColome <nmcolome@gmail.com>
2020-03-06 12:36:11 -0700

    Add /api/v1/invoices#show spec, route & action

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::InvoicesController < ApplicationController
   def index
     render json: InvoicesSerializer.new(Invoice.all)
   end
+
+  def show
+    render json: InvoicesSerializer.new(Invoice.find(params[:id]))
+  end
 end
--- spec/requests/api/v1/invoices_spec.rb
+++ spec/requests/api/v1/invoices_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe "Invoices endpoints" do
 
     expect(invoices.count).to eq(5)
   end
+
+  it "returns a specific invoice based on id" do
+    id = create(:invoice).id
+
+    get "/api/v1/invoices/#{id}"
+
+    expect(response).to be_successful
+
+    raw = JSON.parse(response.body)
+    invoice = raw["data"]
+
+    expect(invoice["id"].to_i).to eq(id)
+    expect(invoice.keys).to eq(%w[id type attributes relationships])
+    expect(invoice["type"]).to eq("invoices")
+    expect(invoice["attributes"].keys).to eq(["status"])
+    expect(invoice["relationships"].keys).to eq(%w[customer merchant transactions invoice_items items])
+  end
 end
