c0a14f528e9cc24e800b2daf77cd0c3ae450c051
NataliaColome <nmcolome@gmail.com>
2020-03-06 20:18:38 -0700

    Add spec, route, RandomController#show for Customer

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       namespace :customers do
         get "/find", to: "search#show"
         get "/find_all", to: "search#index"
+        get "/random", to: "random#show"
       end
 
       namespace :merchants do
