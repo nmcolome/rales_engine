f64d7e37e7955598fbb094ec16c6e1aa33777089
NataliaColome <nmcolome@gmail.com>
2020-03-06 18:55:04 -0700

    Add route, #show & #find_all _items for Items multi finders

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::Items::SearchController < ApplicationController
     render json: ItemsSerializer.new(find_item)
   end
 
+  def index
+    render json: ItemsSerializer.new(find_all_items)
+  end
+
   def find_item
     if params["id"]
       Item.find(params["id"])
@@ -17,6 +21,23 @@ class Api::V1::Items::SearchController < ApplicationController
       Item.find_by(created_at: params["created_at"])
     elsif params["updated_at"]
       Item.find_by(updated_at: params["updated_at"])
+    end
+  end
+
+  def find_all_items
+    if params["id"]
+      Item.find(params["id"])
+    elsif params["name"]
+      Item.where(name: params["name"].capitalize)
+    elsif params["description"]
+      params["description"] = params["description"].split(". ").map{ |e| e.capitalize }.join(". ")
+      Item.where(description: params["description"])
+    elsif params["unit_price"]
+      Item.where(unit_price: params["unit_price"])
+    elsif params["created_at"]
+      Item.where(created_at: params["created_at"])
+    elsif params["updated_at"]
+      Item.where(updated_at: params["updated_at"])
     end
   end
 end
