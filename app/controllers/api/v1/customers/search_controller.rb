a9e293b906cf9426381ef481c6c921ce8e237cfb
NataliaColome <nmcolome@gmail.com>
2020-03-06 17:13:25 -0700

    Refactor if statements to assign variable once

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::Customers::SearchController < ApplicationController
   def show
-    if params["id"]
-      @customer = Customer.find(params["id"])
-    elsif params["first_name"]
-      @customer = Customer.find_by(first_name: params["first_name"])
-    elsif params["last_name"]
-      @customer = Customer.find_by(last_name: params["last_name"])
-    elsif params["created_at"]
-      @customer = Customer.find_by(created_at: params["created_at"])
-    elsif params["updated_at"]
-      @customer = Customer.find_by(updated_at: params["updated_at"])
+    @customer =
+      if params["id"]
+        Customer.find(params["id"])
+      elsif params["first_name"]
+        Customer.find_by(first_name: params["first_name"])
+      elsif params["last_name"]
+        Customer.find_by(last_name: params["last_name"])
+      elsif params["created_at"]
+        Customer.find_by(created_at: params["created_at"])
+      elsif params["updated_at"]
+        Customer.find_by(updated_at: params["updated_at"])
     end
 
     render json: CustomersSerializer.new(@customer)
--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    if params["id"]
-      @merchant = Merchant.find(params["id"])
-    elsif params["name"]
-      @merchant = Merchant.find_by(name: params["name"])
-    elsif params["created_at"]
-      @merchant = Merchant.find_by(created_at: params["created_at"])
-    elsif params["updated_at"]
-      @merchant = Merchant.find_by(updated_at: params["updated_at"])
-    end
+    @merchant =
+      if params["id"]
+        Merchant.find(params["id"])
+      elsif params["name"]
+        Merchant.find_by(name: params["name"])
+      elsif params["created_at"]
+        Merchant.find_by(created_at: params["created_at"])
+      elsif params["updated_at"]
+        Merchant.find_by(updated_at: params["updated_at"])
+      end
 
     render json: MerchantsSerializer.new(@merchant)
   end
